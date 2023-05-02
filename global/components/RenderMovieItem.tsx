b1eacd4fe5e2e8a6b09d3ccb61a2610af6edb4af
Nicolette Pascual <nicolette.pascual@gmail.com>
2023-05-02 19:32:31 +0800

    feat: apply redirection to movie details screen when movie item list is clicked

--- global/components/RenderMovieItem.tsx
+++ global/components/RenderMovieItem.tsx
@@ -2,36 +2,36 @@ import { Image, Text, View } from "react-native";
 import { globalStyles } from "../styles";
 import { styles } from "../../screens/Account/AccountScreen.style";
 import { endpoints } from "../../utils/endpoints";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export function RenderMovieItem({ movie }: { movie: Movie }) {
+  const navigation = useNavigation();
   return (
-    <View style={[styles.movieItemView, globalStyles.row]}>
-      <Image
-        style={styles.poster}
-        // style={{
-        //   margin: 5,
-        //   marginRight: 10,
-        //   flex: 1,
-        //   justifyContent: "center",
-        //   width: 100,
-        //   height: 150,
-        //   aspectRatio: 9 / 16,
-        // }}
-        source={{
-          uri: `${endpoints.images}${movie.poster_path}`,
-        }}
-      />
-      <View style={{ flex: 2 }}>
-        <Text style={styles.movieItemTitle}>{movie.title}</Text>
-        <View style={[globalStyles.row]}>
-          <Text>{movie.release_date}</Text>
-          <Text>{` | ${movie.original_language}`}</Text>
-        </View>
-        <View style={{ position: "absolute", bottom: 15 }}>
-          <Text>Vote Average: {movie.vote_average}</Text>
-          {movie?.rating && <Text>Your Rating: {movie.rating}</Text>}
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("MovieDetails" as never, { movie } as never);
+      }}
+    >
+      <View style={[styles.movieItemView, globalStyles.row]}>
+        <Image
+          style={styles.poster}
+          source={{
+            uri: `${endpoints.images}${movie.poster_path}`,
+          }}
+        />
+        <View style={{ flex: 2 }}>
+          <Text style={styles.movieItemTitle}>{movie.title}</Text>
+          <View style={[globalStyles.row]}>
+            <Text>{movie.release_date}</Text>
+            <Text>{` | ${movie.original_language}`}</Text>
+          </View>
+          <View style={{ position: "absolute", bottom: 15 }}>
+            <Text>Vote Average: {movie.vote_average}</Text>
+            {movie?.rating && <Text>Your Rating: {movie.rating}</Text>}
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
